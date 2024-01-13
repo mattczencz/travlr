b72e9f489a0e3540473f4d3d925fba5e9ecb668f
Matt Czencz <Mattczencz24@gmail.com>
2024-01-13 14:50:35 -0700

    Built navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,11 +1,37 @@
+import { getServerSession } from 'next-auth';
 import Link from 'next/link';
+import authOptions from './auth/authOptions';
+import NavLink from './components/Navigation/NavLink';
 
 const Navbar = () => {
+  const navLinks: { label: string; href: string; }[] = [
+    { label: 'Home', href: '/' },
+    { label: 'About', href: '/about' },
+    { label: 'Explore', href: '/explore' },
+    { label: 'Categories', href: '/categories' },
+  ];
+
   return (
-    <nav>
-      <Link href="/api/auth/signin">Sign In</Link>
-      <Link href="/api/auth/signout">Sign Out</Link>
+    <nav className="flex gap-4 items-center p-6">
+      <Link href="/" className="font-extrabold text-4xl">Travlr</Link>
+      <div className="flex gap-4 flex-1 justify-center">
+        {navLinks.map(link => (
+          <NavLink href={link.href} label={link.label} />
+        ))}
+      </div>
+      <div className="flex">
+        <AuthLinks />
+      </div>
     </nav>
   );
 };
 export default Navbar;
+
+const AuthLinks = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (session)
+    return <Link href="/api/auth/signout" className="nav-btn">Log Out</Link>;
+
+  return <Link href="/api/auth/signin" className="nav-btn">Log In</Link>;
+};
---
+++ app/components/Navigation/NavLink.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import Link from 'next/link';
+import classNames from 'classnames';
+import { usePathname } from 'next/navigation';
+
+interface Props {
+  label: string;
+  href: string;
+}
+
+const NavLink = ({ label, href }: Props) => {
+  const pathname = usePathname();
+  return (
+    <Link
+      key={href}
+      href={href}
+      className={classNames({
+        "active": href === pathname
+      }, "navlink")}
+    >
+      {label}
+    </Link>
+  );
+};
+export default NavLink;
